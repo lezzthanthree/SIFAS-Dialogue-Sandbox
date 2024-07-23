cae0f695e4c6a21428ccfc9e9076898b92698e0b
Smilie Pop <arisatominato48@gmail.com>
2024-07-23 10:16:40 +0800

    Implement Design

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./css/main.css";
+
+function App() {
+    return (
+        <>
+            <input type="range" name="" id="" className="pink"/>
+        </>
+    );
+}
+
+export default App;
