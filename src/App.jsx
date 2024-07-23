e7c59ea4a2f483821bbae5ed3faf7c5235e2e23b
Smilie Pop <arisatominato48@gmail.com>
2024-07-23 13:42:19 +0800

    Created Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,21 @@ import "./css/main.css";
 import data from "./characters.json";
 
 function App() {
-    return <></>;
+    return (
+        <main>
+            <div id="content"></div>
+            <div id="sidebar">
+                <div className="group">
+                    <h1 className="white">Text</h1>
+                    <button className="btn-small w-100 setting">Edit Text</button>
+                </div>
+                <div className="group">
+                    <h1 className="white">Name Tag</h1>
+                    <button className="btn-small w-100 setting">Edit Text</button>
+                </div>
+            </div>
+        </main>
+    );
 }
 
 export default App;
