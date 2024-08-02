493fab11c152fa227544b0214daefbf9c86c4287
Smilie Pop <arisatominato48@gmail.com>
2024-08-02 13:31:49 +0800

    Ignore existing output file

--- public/img/background/low_res.py
+++ public/img/background/low_res.py
@@ -6,4 +6,4 @@ for file in x:
     if "jpg" in file or "png" in file:
         os.system("clear")
         print(f"Processing {file}")
-        os.system(f"ffmpeg -i {file} -vf scale=500:-1 ../background_low/{file}")
+        os.system(f"ffmpeg -n -i {file} -vf scale=500:-1 ../background_low/{file}")
