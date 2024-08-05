63287837bdbd8260b84ec5b0a006688340988ed5
Smilie Pop <arisatominato48@gmail.com>
2024-08-05 22:50:52 +0800

    Added Error Handling

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,39 @@ import Sidebar from "./components/Sidebar";
 import Content from "./components/Content";
 import Announcement from "./components/Announcement";
 import { AppContext } from "./AppContext";
-import "./css/main.css";
 import { useTranslation } from "react-i18next";
+import { ErrorBoundary } from "react-error-boundary";
 
 function App() {
     const { hideState } = useContext(AppContext);
     const { i18n } = useTranslation();
     const language = i18n.language;
     return (
-        <main className={["en", "es"].includes(language) ? "latin" : "cjk"}>
-            <Announcement></Announcement>
-            <Content></Content>
-            {hideState === false && <Sidebar></Sidebar>}
-        </main>
+        <ErrorBoundary fallbackRender={ErrorFallback}>
+            <main className={["en", "es"].includes(language) ? "latin" : "cjk"}>
+                <Announcement></Announcement>
+                <Content></Content>
+                {hideState === false && <Sidebar></Sidebar>}
+            </main>
+        </ErrorBoundary>
+    );
+}
+
+function ErrorFallback({ error}) {
+    return (
+        <div className="center top-0-0 full-screen column latin">
+            <img src="/img/error.png" />
+            <h1>Something went wrong!</h1>
+            <p className="bottom-10">An unexpected error has occured.</p>
+            <pre className="error-message">{error.message}</pre>
+            <a
+                href="https://github.com/lezzthanthree/SIFAS-Dialogue-Sandbox/blob/master/README.md#report-an-issue"
+                target="_blank"
+            >
+                Report this issue on GitHub.
+            </a>
+            <button></button>
+        </div>
     );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -7,10 +7,15 @@ import { I18nextProvider } from "react-i18next";
 import i18n from "i18next";
 import { AppProvider } from "./AppContext.jsx";
 import i18nInit from "./js/i18ninit.js";
+import "./css/main.css";
 
 i18nInit();
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root"), {
+    onUncaughtError: (error, errorInfo) => {
+        console.error("Found you!", error, errorInfo.componentStack);
+    },
+}).render(
     <React.StrictMode>
         <AppProvider>
             <I18nextProvider i18n={i18n}>
