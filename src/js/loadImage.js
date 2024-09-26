ef74b14c629cb849b58cda296a64bde6b650c9e4
Smilie Pop <arisatominato48@gmail.com>
2024-09-26 10:28:57 +0800

    Show alert on error

--- src/js/loadImage.js
+++ src/js/loadImage.js
@@ -1,10 +1,17 @@
 const loadImage = async (src) => {
-    return new Promise((resolve, reject) => {
+    const promise = new Promise((resolve, reject) => {
         const img = new Image();
         img.src = src;
         img.onload = () => resolve(img);
-        img.onerror = () => reject(new Error(`Failed to load image: ${src}`));
+        img.onerror = () => {
+            alert(
+                `Something went wrong. It may be a timeout or the file does not exist...\nHere's the src image: ${src}`
+            );
+            reject(new Error(`Failed to load image: ${src}`));
+        };
     });
+    console.log(promise);
+    return promise;
 };
 
 export default loadImage;
